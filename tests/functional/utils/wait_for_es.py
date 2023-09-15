21c8da721f9c740977874ab3fe0b321335c15090
vraja <vraja87@bk.ru>
2023-09-15 12:03:53 +0300

    fix. wait for healthcheck elastic container

--- tests/functional/utils/wait_for_es.py
+++ tests/functional/utils/wait_for_es.py
@@ -9,7 +9,12 @@ if __name__ == '__main__':
     es_client = Elasticsearch(hosts='http://elasticsearch:9200')
     while True:
         response = es_client.ping()
-        if response:
+        if not response:
+            continue
+        health_response = es_client.cluster.health(wait_for_status="yellow", timeout="20s")
+        if not health_response:
+            continue
+        if health_response['status'] == 'yellow':
             break
         time.sleep(1)
     es_client.close()
