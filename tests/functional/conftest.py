ec119e7da8c743c87016d5a430507ddc65d6ac54
vraja <vraja87@bk.ru>
2023-09-19 19:12:45 +0300

    review. crop conftest to several files using pytest_plugins

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,88 +1,14 @@
 import asyncio
+import pytest_asyncio
 import time
 
 import aiohttp
 import pytest
-import json
 
-from redis.asyncio import Redis
-
-import pytest_asyncio
-from elasticsearch import AsyncElasticsearch
-from elasticsearch.helpers import async_bulk
-from loguru import logger
-import functional.testdata.es_backup as es_mapping
 from functional.settings import test_settings
 
 
-@pytest.fixture(scope='session')
-def redis_cleanup():
-    """Cleanup redis cache."""
-    async def inner():
-        redis_client = Redis.from_url(test_settings.redis_host)
-        await redis_client.flushall()
-    return inner
-
-
-def get_es_bulk_query(es_data: list[dict], es_index: str, es_id_field: str):
-    bulk_query = []
-    for row in es_data:
-        bulk_query.extend([
-            json.dumps({'index': {'_index': es_index,
-                                  '_id': row[es_id_field]}}),
-            json.dumps(row)
-        ])
-    return bulk_query
-
-
-@pytest_asyncio.fixture(scope="session", autouse=True)
-async def es_fill_indexes():
-    """
-    Asynchronously create and fill Elasticsearch indices for testing.
-
-    This fixture runs automatically before the test session and
-    - Creates an Elasticsearch client
-    - Checks if the index exists; if so, deletes it
-    - Creates a new index with settings and mappings
-    - Fills the index with data
-    - Refreshes all indices
-
-    Yields:
-        client: The Elasticsearch client.
-    """
-
-
-    logger.info("Create elasticsearch client.")
-
-    async with AsyncElasticsearch(hosts=[test_settings.es_host]) as client:
-        for index_name in test_settings.es_indexes:
-            if await client.indices.exists(index=index_name):
-                await client.indices.delete(index=index_name)
-            logger.info(f"Create {index_name} index.")
-            await client.indices.create(index=index_name,
-                                        settings=es_mapping.index[index_name]['settings'],
-                                        mappings=es_mapping.index[index_name]['mappings'])
-
-        for index_name in test_settings.es_indexes:
-            logger.info(f"Fill {index_name} index.")
-            actions = [
-                {
-                    "_index": index_name,
-                    "_id": doc.get("uuid", None),
-                    "_source": doc
-                }
-                for doc in es_mapping.data[index_name]
-            ]
-            await async_bulk(client, actions)
-
-        await client.indices.refresh(index="_all")
-        for index_name in test_settings.es_indexes:
-            await client.cluster.health(index=index_name, wait_for_status='yellow')
-
-        yield client
-        for index in test_settings.es_indexes:
-            logger.info(f"Clear {index_name} index.")
-            await client.options(ignore_status=[400, 404]).indices.delete(index=index)
+pytest_plugins = ("functional.fixtures.redis", "functional.fixtures.elastic")
 
 
 @pytest.fixture(scope="session")
---
+++ tests/functional/fixtures/redis.py
@@ -0,0 +1,14 @@
+from redis.asyncio import Redis
+import pytest_asyncio
+from functional.settings import test_settings
+
+
+@pytest_asyncio.fixture(scope='session')
+def redis_cleanup():
+    """Cleanup redis cache."""
+    async def inner():
+        redis_client = Redis.from_url(test_settings.redis_host)
+        await redis_client.flushall()
+    return inner
+
+
